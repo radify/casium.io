c631a1edce8ae96231c32142b7d3597683c63e2c
Michael J. Wiebe <wiebe.michael.j@gmail.com>
2017-10-25 16:11:34 -0400

    added biz advantages, progess on styling

--- scripts.js
+++ scripts.js
@@ -12,34 +12,37 @@ var reduxBtn = $('.redux-btn');
 var casiumBtn = $('.casium-btn');
 
 comparisonBtnContainer.click(() => {
-	reduxExample.toggleClass('block');
-	casiumExample.toggleClass('block');
-	reduxBtn.toggleClass('block');
-	casiumBtn.toggleClass('block');
+  reduxExample.toggleClass('block');
+  casiumExample.toggleClass('block');
+  reduxBtn.toggleClass('block');
+  casiumBtn.toggleClass('block');
 });
 
 window.addEventListener('scroll', function(e) {
-	toggleNav(window.scrollY);
-	fadeElementsIn();
+  toggleNav(window.scrollY);
+  fadeElementsIn();
 });
 hamburgerBtn.click(function() {
-	$('.nav').toggleClass('hamburger-show');
-	hamburgerBtnOpen.toggleClass('hamburger-btn-show');
-	hamburgerBtnClose.toggleClass('hamburger-btn-show');
+  $('.nav').toggleClass('hamburger-show');
+  hamburgerBtnOpen.toggleClass('hamburger-btn-show');
+  hamburgerBtnClose.toggleClass('hamburger-btn-show');
 });
 function fadeElementsIn() {
-	[].forEach.call(whyReasons, (reason, index) => {
-		var reasonBounds = reason.getBoundingClientRect();
-		if (window.scrollY > reasonBounds.bottom) {
-			whyReasons[index].style.opacity = 1;
-		}
-	});
+  [].forEach.call(whyReasons, (reason, index) => {
+    var reasonBounds = reason.getBoundingClientRect();
+    if (window.scrollY > reasonBounds.bottom) {
+      whyReasons[index].style.opacity = 1;
+    }
+  });
 }
 function toggleNav(currentScrollY) {
-	if (lastScrollY > 500 && lastScrollY < currentScrollY) {
-		header.addClass('slideOff');
-	} else {
-		header.removeClass('slideOff');
-	}
-	lastScrollY = currentScrollY;
+  if (lastScrollY > 500 && lastScrollY < currentScrollY) {
+    header.addClass('slideOff');
+  } else {
+    header.removeClass('slideOff');
+    // setTimeout(() => {
+    //   header.addClass('slideOff');
+    // }, 5000);
+  }
+  lastScrollY = currentScrollY;
 }
